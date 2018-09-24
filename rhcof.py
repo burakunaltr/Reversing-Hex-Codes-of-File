f9e09e3321de0b20d35f4c8e52e69c1462c941b9
Burak Ünal <burakunalofficial@gmail.com>
2018-09-24 14:35:10 +0300

    Add files via upload

---
+++ rhcof.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import binascii
+
+print("Hex Kodlarını Ters Çevireceğiniz Dosyayı Giriniz..")
+dosya = str(input("Dosya : "))
+with open(dosya,'rb') as f: #Gösterilen dosya okunur.
+    icerik = f.read()
+hexDump = str(binascii.b2a_hex(icerik)) #Dosya içeriği hexkodlarına dönüştürülüp string olarak değişkene atanır.
+
+liste = [hexDump[i:i+2] for i in range(0, len(hexDump), 2)] #String değişkeni 2 karakterli gruplar halinde liste değişkenine atanır.
+reverseHex = ''.join(liste[::-1]) #Liste tersten başlayarak string değişkeninde birleştirilir.
+reverseHex = reverseHex[1:-2] #Stringin başında ve sonundaki gereksiz karakterlerden kurtulunur.
+
+print("Lütfen Çıktı için Uzantısıyla Birlikte Dosya Adı Giriniz..")
+cikti = str(input("Çıktı : "))
+with open(cikti,'wb') as fout: #Çıktı için dosya oluşturulur
+    fout.write(binascii.a2b_hex(reverseHex)) #Ters çevrilmiş hex kodları dosyaya yazılır.
+
+print("O iş Tamam ;)")
